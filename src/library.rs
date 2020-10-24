ac163e600ce1503295cda02f53207e096ab5e2c1
Henrik Friedrichsen <henrik@affekt.org>
2020-10-24 20:31:05 +0200

    use local playlist store to check for duplicates

--- src/library.rs
+++ src/library.rs
@@ -525,27 +525,14 @@ impl Library {
 
     // Return true if the given playlist contains the given track
     pub fn playlist_has_track(&self, playlist_id: &str, track_id: &str) -> bool {
-        let playlist = match self.spotify.playlist(playlist_id) {
-            Some(p) => Playlist::from(&p),
-            None => return false,
-        };
-
-        playlist
-            .get_all_tracks(Arc::clone(&self.spotify))
-            .iter()
-            .any(|track| {
-                track
-                    .id
-                    .as_ref()
-                    .and_then(|id| {
-                        if id.to_string() == track_id.to_string() {
-                            Some(())
-                        } else {
-                            None
-                        }
-                    })
-                    .is_some()
+        let playlists = self.playlists.read().expect("can't readlock playlists");
+        if let Some(playlist) = playlists.iter().find(|p| p.id == playlist_id) {
+            playlist.tracks.as_ref().map_or(false, |tracks| {
+                tracks.iter().any(|t| t.id == Some(track_id.to_string()))
             })
+        } else {
+            false
+        }
     }
 
     pub fn playlist_append_tracks(&self, playlist_id: &str, new_tracks: &[Track]) {
