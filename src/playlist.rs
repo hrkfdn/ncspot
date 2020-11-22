ddf6064675aee4f63f65fc453a05f86f04047bee
André Andersson <andre.eric.andersson@gmail.com>
2020-11-22 15:49:36 +0100

    Only show playlists where the user is allowed to add tracks

--- src/playlist.rs
+++ src/playlist.rs
@@ -19,6 +19,7 @@ pub struct Playlist {
     pub snapshot_id: String,
     pub num_tracks: usize,
     pub tracks: Option<Vec<Track>>,
+    pub collaborative: bool
 }
 
 impl Playlist {
@@ -126,6 +127,7 @@ impl From<&SimplifiedPlaylist> for Playlist {
             snapshot_id: list.snapshot_id.clone(),
             num_tracks,
             tracks: None,
+            collaborative: list.collaborative,
         }
     }
 }
@@ -139,6 +141,7 @@ impl From<&FullPlaylist> for Playlist {
             snapshot_id: list.snapshot_id.clone(),
             num_tracks: list.tracks.total as usize,
             tracks: None,
+            collaborative: list.collaborative
         }
     }
 }
--- src/ui/contextmenu.rs
+++ src/ui/contextmenu.rs
@@ -38,9 +38,12 @@ impl ContextMenu {
         track: Track,
     ) -> Modal<Dialog> {
         let mut list_select: SelectView<Playlist> = SelectView::new().autojump();
+        let current_user_id = spotify.current_user().unwrap().id;
 
         for list in library.items().iter() {
-            list_select.add_item(list.name.clone(), list.clone());
+            if current_user_id == list.owner_id || list.collaborative {
+                list_select.add_item(list.name.clone(), list.clone());
+            }
         }
 
         list_select.set_on_submit(move |s, selected| {
