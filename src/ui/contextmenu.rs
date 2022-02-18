a491a20de207526c05040291316eaed352a2084d
Henrik Friedrichsen <henrik@affekt.org>
2022-02-18 22:25:36 +0100

    Enable autojump for playlist selection list
    It's not a full search, but a quick win
    
    #225

--- src/ui/contextmenu.rs
+++ src/ui/contextmenu.rs
@@ -58,6 +58,7 @@ impl ContextMenu {
             }
         }
 
+        list_select.set_autojump(true);
         list_select.set_on_submit(move |s, selected| {
             let track = track.clone();
             let mut playlist = selected.clone();
