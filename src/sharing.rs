5158a13e19fbcf516b9816fdf75240106d43bf99
Bettehem <chris.mustola@gmail.com>
2022-07-26 00:05:13 +0300

    Enable wayland clipboard support by default on supported platforms (#869)
    * Added possibility to Follow/Unfollow artists via the context menu
    
    * Formatted code properly.
    
    * Wayland support is now automatically built on supported platforms.
    
    * Revert "Formatted code properly."
    
    This reverts commit ec057f9716574d0a7912ba8b5308c73bb0342f84.
    
    * Revert "Added possibility to Follow/Unfollow artists via the context menu"
    
    This reverts commit 9ac20514a640b9150fcfa38630d338ad6f097689.
    
    * Removed need for separate wayland_clipboard feature.
    Possibly fixed windows build.
    
    * Added more cfg conditions to sharing.rs.
    This possibly fixes the windows build.
    
    * Changed to only build wayland clipboard feature on linux by default.

--- src/sharing.rs
+++ src/sharing.rs
@@ -1,7 +1,7 @@
 #![cfg(feature = "share_clipboard")]
 use std::env;
 
-#[cfg(feature = "wayland_clipboard")]
+#[cfg(all(feature = "wl-clipboard-rs", target_os = "linux"))]
 use {
     std::io::Read,
     wl_clipboard_rs::{
@@ -14,7 +14,10 @@ use {
 
 #[cfg(feature = "share_selection")]
 use clipboard::{x11_clipboard, x11_clipboard::X11ClipboardContext};
-#[cfg(all(feature = "share_selection", feature = "wayland_clipboard"))]
+#[cfg(all(
+    feature = "share_selection",
+    all(feature = "wl-clipboard-rs", target_os = "linux")
+))]
 use wl_clipboard_rs::utils::{is_primary_selection_supported, PrimarySelectionCheckError};
 
 #[cfg(not(feature = "share_selection"))]
@@ -39,7 +42,7 @@ pub fn read_share() -> Option<String> {
     if is_wayland() {
         #[allow(unused_mut, unused_assignments)]
         let mut string = None;
-        #[cfg(feature = "wayland_clipboard")]
+        #[cfg(all(feature = "wl-clipboard-rs", target_os = "linux"))]
         {
             //use wayland clipboard
             let result = get_contents(
@@ -77,7 +80,7 @@ pub fn read_share() -> Option<String> {
     if is_wayland() {
         #[allow(unused_mut, unused_assignments)]
         let mut string = None;
-        #[cfg(feature = "wayland_clipboard")]
+        #[cfg(all(feature = "wl-clipboard-rs", target_os = "linux"))]
         {
             //use wayland clipboard
             string = match is_primary_selection_supported() {
@@ -138,7 +141,7 @@ pub fn write_share(url: String) -> Option<()> {
     if is_wayland() {
         #[allow(unused_mut, unused_assignments)]
         let mut option = None;
-        #[cfg(feature = "wayland_clipboard")]
+        #[cfg(all(feature = "wl-clipboard-rs", target_os = "linux"))]
         {
             //use wayland clipboard
             let opts = Options::new();
@@ -167,7 +170,7 @@ pub fn write_share(url: String) -> Option<()> {
     if is_wayland() {
         #[allow(unused_mut, unused_assignments)]
         let mut option = None;
-        #[cfg(feature = "wayland_clipboard")]
+        #[cfg(all(feature = "wl-clipboard-rs", target_os = "linux"))]
         {
             //use wayland clipboard
             let mut opts = Options::new();
