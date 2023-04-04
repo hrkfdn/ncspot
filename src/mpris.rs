c3ab1ab26566eeed6691882366b741b69bfd0bd7
Henrik Friedrichsen <henrik@affekt.org>
2023-04-04 14:29:54 +0200

    Add missing import

--- src/mpris.rs
+++ src/mpris.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::error::Error;
 use std::sync::Arc;
+use std::time::Duration;
 use tokio::sync::mpsc;
 use tokio_stream::wrappers::UnboundedReceiverStream;
 use tokio_stream::StreamExt;
