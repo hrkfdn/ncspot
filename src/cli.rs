62f879c405706fe397a665a78df48625fb0a0221
Thomas Frans <franske2000@gmail.com>
2023-11-09 22:13:10 +0100

    fix: don't print runtime path on Windows
    Windows doesn't use the runtime path so it shouldn't be printed there.

--- src/cli.rs
+++ src/cli.rs
@@ -1,13 +1,9 @@
-use crate::{
-    config::{user_cache_directory, user_configuration_directory},
-    utils::user_runtime_directory,
-};
+use crate::config::{user_cache_directory, user_configuration_directory};
 
 /// Print platform info like which platform directories will be used.
 pub fn info() -> Result<(), String> {
     let user_configuration_directory = user_configuration_directory();
     let user_cache_directory = user_cache_directory();
-    let user_runtime_directory = user_runtime_directory();
 
     println!(
         "USER CONFIGURATION PATH: {}",
@@ -21,12 +17,19 @@ pub fn info() -> Result<(), String> {
             .map(|path| path.to_string_lossy().to_string())
             .unwrap_or("not found".into())
     );
-    println!(
-        "USER RUNTIME PATH: {}",
-        user_runtime_directory
-            .map(|path| path.to_string_lossy().to_string())
-            .unwrap_or("not found".into())
-    );
+
+    #[cfg(unix)]
+    {
+        use crate::utils::user_runtime_directory;
+
+        let user_runtime_directory = user_runtime_directory();
+        println!(
+            "USER RUNTIME PATH: {}",
+            user_runtime_directory
+                .map(|path| path.to_string_lossy().to_string())
+                .unwrap_or("not found".into())
+        );
+    }
 
     Ok(())
 }
