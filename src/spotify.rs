8f317b2ee7686f48bc06814ae1db653d0679bcad
Thomas Frans <franske2000@gmail.com>
2024-01-28 22:32:01 +0100

    docs: small fixes to the documentation update

--- src/spotify.rs
+++ src/spotify.rs
@@ -31,7 +31,7 @@ use crate::spotify_worker::{Worker, WorkerCommand};
 /// percent.
 pub const VOLUME_PERCENT: u16 = ((u16::max_value() as f64) * 1.0 / 100.0) as u16;
 
-/// Events sent by the
+/// Events sent by the [Player].
 #[derive(Clone, Debug, PartialEq, Eq, Serialize)]
 pub enum PlayerEvent {
     Playing(SystemTime),
