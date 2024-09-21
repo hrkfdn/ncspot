a1fb795a9580f36ea9b60a612dcbff5de2b96b83
Henrik Friedrichsen <henrik@affekt.org>
2024-09-21 11:48:14 +0200

    chore: Use `send_mpris()` and `Duration` for conversion

--- src/spotify.rs
+++ src/spotify.rs
@@ -433,13 +433,15 @@ impl Spotify {
 
     /// Send a Seeked signal on Mpris interface
     #[cfg(feature = "mpris")]
-    pub fn notify_seeked(&self, position: u32) {
-        if let Some(mpris_manager) = self.mpris.lock().unwrap().as_ref() {
-            info!("Seeked event");
-            // Mpris spec requires microseconds
-            let new_position = position * 1000;
-            mpris_manager.send(MprisCommand::EmitSeekedStatus(new_position.into()));
-        }
+    pub fn notify_seeked(&self, position_ms: u32) {
+        let new_position = Duration::from_millis(position_ms.into());
+        let command = MprisCommand::EmitSeekedStatus(
+            new_position
+                .as_micros()
+                .try_into()
+                .expect("track position exceeds MPRIS datatype"),
+        );
+        self.send_mpris(command);
     }
 
     /// Set the current volume of the [Player]. If `notify` is true, also notify MPRIS clients about
