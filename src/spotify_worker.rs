7f451884a768d684a97bff7a2f28c9497b112be2
Henrik Friedrichsen <henrik@affekt.org>
2025-08-28 12:06:11 +0200

    feat: Use Spirc for player logic
    **Heavily WIP, only does basic playback so far**
    
    The idea is to use librespot `Spirc` instead of handling the player logic within
    ncspot. This should reduce complexity a bit, but also allows us to integrate
    with Spotify Connect, which in turn would enable ncspot to be controlled via
    Spotify Apps.

--- src/spotify_worker.rs
+++ src/spotify_worker.rs
@@ -2,11 +2,14 @@ use crate::events::{Event, EventManager};
 use crate::model::playable::Playable;
 use crate::queue::QueueEvent;
 use crate::spotify::PlayerEvent;
+use librespot_connect::{ConnectConfig, LoadRequest, LoadRequestOptions, Spirc};
 use librespot_core::SpotifyUri;
 use librespot_core::session::Session;
 use librespot_playback::mixer::Mixer;
 use librespot_playback::player::{Player, PlayerEvent as LibrespotPlayerEvent};
 use log::{debug, error, info, warn};
+use std::future::Future;
+use std::pin::Pin;
 use std::sync::Arc;
 use std::time::Duration;
 use std::time::SystemTime;
@@ -37,29 +40,37 @@ pub struct Worker {
     events: EventManager,
     player_events: UnboundedReceiverStream<LibrespotPlayerEvent>,
     commands: UnboundedReceiverStream<WorkerCommand>,
-    session: Session,
-    player: Arc<Player>,
     player_status: PlayerStatus,
-    mixer: Arc<dyn Mixer>,
+    session: Session,
+    spirc: Spirc,
+    spirc_task: Pin<Box<dyn Future<Output = ()> + Send>>,
 }
 
 impl Worker {
-    pub(crate) fn new(
+    pub(crate) async fn new(
         events: EventManager,
+        credentials: librespot_core::authentication::Credentials,
         player_events: mpsc::UnboundedReceiver<LibrespotPlayerEvent>,
         commands: mpsc::UnboundedReceiver<WorkerCommand>,
         session: Session,
         player: Arc<Player>,
         mixer: Arc<dyn Mixer>,
     ) -> Self {
+        let config = ConnectConfig {
+            name: "ncspot".to_string(),
+            ..Default::default()
+        };
+        let (spirc, spirc_task) = Spirc::new(config, session.clone(), credentials, player, mixer)
+            .await
+            .expect("Spirc should be initialized");
         Self {
             events,
             player_events: UnboundedReceiverStream::new(player_events),
             commands: UnboundedReceiverStream::new(commands),
-            player,
-            session,
             player_status: PlayerStatus::Stopped,
-            mixer,
+            session,
+            spirc,
+            spirc_task: Box::pin(spirc_task),
         }
     }
 
@@ -76,6 +87,9 @@ impl Worker {
             tokio::select! {
                 cmd = self.commands.next() => match cmd {
                     Some(WorkerCommand::Load(playable, start_playing, position_ms)) => {
+                        if let Err(e) = self.spirc.activate() {
+                            warn!("error activating spirc: {e:?}");
+                        }
                         match SpotifyUri::from_uri(&playable.uri()) {
                             Ok(uri) => {
                                 info!("player loading track: {uri:?}");
@@ -83,7 +97,18 @@ impl Worker {
                                     warn!("track is not playable");
                                     self.events.send(Event::Player(PlayerEvent::FinishedTrack));
                                 } else {
-                                    self.player.load(uri, start_playing, position_ms);
+                                    let options = LoadRequestOptions {
+                                        start_playing,
+                                        seek_to: position_ms,
+                                        context_options: None,
+                                        playing_track: None,
+                                    };
+                                    let req =
+                                        LoadRequest::from_tracks(vec![uri.to_uri().expect("uri")], options);
+                                    if let Err(e) = self.spirc.load(req) {
+                                        error!("error loading track into spirc: {e:?}");
+                                        self.events.send(Event::Player(PlayerEvent::FinishedTrack));
+                                    }
                                 }
                             }
                             Err(e) => {
@@ -93,29 +118,37 @@ impl Worker {
                         }
                     }
                     Some(WorkerCommand::Play) => {
-                        self.player.play();
+                        if let Err(e) = self.spirc.play() {
+                            error!("error resuming spirc: {e:?}");
+                        }
                     }
                     Some(WorkerCommand::Pause) => {
-                        self.player.pause();
+                        if let Err(e) = self.spirc.pause() {
+                            error!("error pausing spirc: {e:?}");
+                        }
                     }
                     Some(WorkerCommand::Stop) => {
-                        self.player.stop();
+                        //todo!("stop spirc");
                     }
                     Some(WorkerCommand::Seek(pos)) => {
-                        self.player.seek(pos);
+                        if let Err(e) = self.spirc.set_position_ms(pos) {
+                            error!("error seeking spirc: {e:?}");
+                        }
                     }
                     Some(WorkerCommand::SetVolume(volume)) => {
-                        self.mixer.set_volume(volume);
+                        if let Err(e) = self.spirc.set_volume(volume) {
+                            error!("error setting spirc volume: {e:?}");
+                        }
                     }
                     Some(WorkerCommand::Preload(playable)) => {
                         if let Ok(uri) = SpotifyUri::from_uri(&playable.uri()) {
                             debug!("Preloading {uri:?}");
-                            self.player.preload(uri);
                         }
                     }
                     Some(WorkerCommand::Shutdown) => {
-                        self.player.stop();
-                        self.session.shutdown();
+                        if let Err(e) = self.spirc.shutdown() {
+                            error!("error shutting down spirc: {e:?}");
+                        }
                     }
                     None => info!("empty stream")
                 },
@@ -172,6 +205,9 @@ impl Worker {
                         break
                     },
                 },
+                _ = self.spirc_task.as_mut() => {
+                    info!("spirc task tick");
+                },
                 // Update animated parts of the UI (e.g. statusbar during playback).
                 _ = ui_refresh.tick() => {
                     if !matches!(self.player_status, PlayerStatus::Stopped) {
@@ -186,6 +222,6 @@ impl Worker {
 impl Drop for Worker {
     fn drop(&mut self) {
         debug!("Worker thread is shutting down, stopping player");
-        self.player.stop();
+        let _ = self.spirc.shutdown();
     }
 }
